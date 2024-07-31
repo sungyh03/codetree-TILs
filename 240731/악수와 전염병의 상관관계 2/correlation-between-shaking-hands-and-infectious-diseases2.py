c66f3427180f1344769a15101da4036707b79230
sungyh03 <sungyh90@gmail.com>
2024-07-31 22:26:25 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240731/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240731/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -7,7 +7,7 @@ for _ in range(T):
     infomation.append(tuple(map(int,input().split())))
 infomation.sort()
 for _,x,y in infomation:
-    if (infection[x-1] == 1 and infected_handshake_cnt[x-1]< K) and infection[y-1] == 1 and infected_handshake_cnt[y-1]< K :
+    if (infection[x-1] == 1 and infected_handshake_cnt[x-1]< K) and (infection[y-1] == 1 and infected_handshake_cnt[y-1]< K) :
         infected_handshake_cnt[x-1]+=1
         infected_handshake_cnt[y-1]+=1
     elif (infection[x-1] == 1 and infected_handshake_cnt[x-1]< K) and infection[y-1] == 0:
@@ -16,7 +16,12 @@ for _,x,y in infomation:
     elif (infection[y-1] == 1 and infected_handshake_cnt[y-1]< K) and infection[x-1]==0:
         infection[x-1] = 1
         infected_handshake_cnt[y-1]+=1
-    else:
-        continue
+    elif infection[x-1] == 1 and infection[y-1] == 1:
+        infected_handshake_cnt[x-1] += 1
+        infected_handshake_cnt[y-1] += 1
+    elif infection[x-1] == 1:
+        infected_handshake_cnt[x-1] += 1
+    elif infection[y-1] == 1:
+        infected_handshake_cnt[y-1] += 1
 for i in infection:
     print(i,end='')
