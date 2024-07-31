6c4bf9039dc45ead726d0c106f0aa98c93c487fd
sungyh03 <sungyh90@gmail.com>
2024-07-31 22:16:54 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240731/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -7,11 +7,16 @@ for _ in range(T):
     infomation.append(tuple(map(int,input().split())))
 infomation.sort()
 for _,x,y in infomation:
-    if infection[x-1] == 1 and infected_handshake_cnt[x-1]< K:
+    if (infection[x-1] == 1 and infected_handshake_cnt[x-1]< K) and infection[y-1] == 1 and infected_handshake_cnt[y-1]< K :
+        infected_handshake_cnt[x-1]+=1
+        infected_handshake_cnt[y-1]+=1
+    elif (infection[x-1] == 1 and infected_handshake_cnt[x-1]< K) and infection[y-1] == 0:
         infection[y-1] = 1
-        infected_handshake_cnt[x-1] += 1
-    if infection[y-1] == 1 and infected_handshake_cnt[y-1]< K:
+        infected_handshake_cnt[x-1]+=1
+    elif (infection[y-1] == 1 and infected_handshake_cnt[y-1]< K) and infection[x-1]==0:
         infection[x-1] = 1
-        infected_handshake_cnt[y-1] += 1
+        infected_handshake_cnt[y-1]+=1
+    else:
+        continue
 for i in infection:
     print(i,end='')
