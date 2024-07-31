1abca790947e47bde95372e5ec8d019e17ecd3d0
sungyh03 <sungyh90@gmail.com>
2024-07-31 21:00:24 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,19 @@
+N,K,P,T = tuple(map(int,input().split()))
+infected_handshake_cnt = [ 0 for _ in range(N)]
+infection = [ 0 for _ in range(N)]
+infection[P-1] = 1
+infomation = []
+for _ in range(T):
+    infomation.append(tuple(map(int,input().split())))
+infomation.sort()
+for _,x,y in infomation:
+    if infection[x-1] == 1 and infected_handshake_cnt[x-1]< K:
+        infection[x-1] = 1
+        infection[y-1] = 1
+        infected_handshake_cnt[x-1] += 1
+    if infection[y-1] == 1 and infected_handshake_cnt[y-1]< K:
+        infection[x-1] = 1
+        infection[y-1] = 1
+        infected_handshake_cnt[y-1] += 1
+for i in infection:
+    print(i,end='')
