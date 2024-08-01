1047942347412db02a11b33b8463282ff7867e40
sungyh03 <sungyh90@gmail.com>
2024-08-01 19:55:31 +0900

    Title: 되돌아오기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240801/되돌아오기/come-back.py
@@ -0,0 +1,21 @@
+x,y = 0,0
+dxs,dys = [1,-1,0,0],[0,0,-1,1]
+move_dir = -1
+dir_dict ={'E':0,'W':1,'S':2,'N':3} 
+elapsed_t = 0
+arrived = False
+N = int(input())
+for _ in range(N):
+    d,t = input().split()
+    t = int(t)
+    move_dir = dir_dict[d]
+    for _ in range(t):
+        x,y = x+dxs[move_dir],y+dys[move_dir]
+        elapsed_t += 1
+        if x == 0 and y == 0:
+            arrived = True
+            answer = elapsed_t
+if arrived:
+    print(answer)
+else:
+    print(-1)
